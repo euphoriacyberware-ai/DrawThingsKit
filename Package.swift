bd218a0a1ae55a2a36eeaf3ac92e926754e4becb
euphoriacyberware-ai <brian.cantin@gmail.com>
2025-11-29 13:16:44 -0500

    Reset DrawThingsClient dependency to latest v1.1.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/euphoriacyberware-ai/DT-gRPC-Swift-Client.git", branch: "ios-compatibility"),
+        .package(url: "https://github.com/euphoriacyberware-ai/DT-gRPC-Swift-Client.git", from: "1.1.1"),
     ],
     targets: [
         .target(
